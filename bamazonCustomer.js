97df5f65a3cdb1efecba6c03cdc99cf87cfe68a9
cczapata <zapatacynthia@gmail.com>
2018-02-25 19:46:26 -0800

    updated js file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,17 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 
-var 
+var connection = mysql.createConnection({
+  host: "localhost",
+  port:
+
+  user:"root"
+
+  password:""
+  database:"bamazon_db"
+});
+
+connection.connect(function(err)){
+  if(err) throw err;
+  console.log("")
+}
